20626ebd97a5c22df7cf8f360e294b5ed00a8ece
sigsec <me@sigsec.net>
2019-03-06 23:27:42 +1100

    Add telstra_messaging_api.py

---
+++ telstra_messaging_api.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# Code for article:
+# http://blog.sigsec.net/posts/2019/03/sending-smses-from-python.html
+#
+
+import requests
+
+
+def telstra_request(endpoint, body=None, headers=None, *, token=None, method='POST'):
+    send_headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    if token:  # if this is an authenticated request, add the token header
+        send_headers['Authorization'] = 'Bearer ' + token
+    if headers:  # add any extra headers if desired
+        send_headers.update(headers)
+
+    url = "https://tapi.telstra.com/v2/" + endpoint
+    return requests.request(method, url, json=body, headers=send_headers)
+
+
+def auth(app_key, app_secret):
+    body = dict(client_id=app_key, client_secret=app_secret,
+        grant_type="client_credentials", scope="NSMS")
+    response = requests.post("https://tapi.telstra.com/v2/oauth/token", body)
+
+    if response.status_code != 200:
+        raise RuntimeError("Bad response from Telstra API! " + response.text)
+
+    response_json = response.json()
+    return response_json['access_token']
+
+
+def create_subscription(token):
+    response = telstra_request("messages/provisioning/subscriptions", {}, token=token)
+
+    if response.status_code != 201:
+        raise RuntimeError("Bad response from Telstra API! " + response.text)
+
+    response_json = response.json()
+    return response_json['destinationAddress']
+
+
+def send_sms(token, to, body):
+    payload = dict(to=to, body=body)
+    response = telstra_request("messages/sms", payload, token=token)
+
+    if response.status_code != 201:
+        raise RuntimeError("Bad response from Telstra API! " + response.text)
+
+    response_json = response.json()
+    return response_json
+
+
+#
+# Main function, for when this file is run as a script
+#
+
+def main():
+    token = auth("PutYour32CharClientKeyHereThanks", "ClientSecretHere")
+    create_subscription(token)
+    send_sms(token, "+61400000000", "Hello sigsec readers!")
+
+
+if __name__ == '__main__':
+    main()
+
